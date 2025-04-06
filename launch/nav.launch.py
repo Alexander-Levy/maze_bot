23a78137d6af2601ee8f2ef78c6068c4ec8cf7ec
Alex Levy <138838834+Alexander-Levy@users.noreply.github.com>
2025-04-06 09:43:33 -0400

    Updated navigation files

--- launch/nav.launch.py
+++ launch/nav.launch.py
@@ -1,150 +1,124 @@
 import os
-from launch_ros.actions import Node
-from launch import LaunchDescription
-from nav2_common.launch import RewrittenYaml
-from launch_ros.actions import LoadComposableNodes
-from launch.substitutions import LaunchConfiguration
-from launch_ros.descriptions import ComposableNode, ParameterFile
+
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from nav2_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
+    # Get the launch directory
+    bringup_dir = get_package_share_directory('maze_bot')
 
-    # Package name
-    package_name = get_package_share_directory('maze_bot')
-
-    # Launch configurations    
+    namespace = LaunchConfiguration('namespace')
+    use_sim_time = LaunchConfiguration('use_sim_time')
     autostart = LaunchConfiguration('autostart')
     params_file = LaunchConfiguration('params_file')
-    use_sim_time = LaunchConfiguration('use_sim_time')
+    default_bt_xml_filename = LaunchConfiguration('default_bt_xml_filename')
+    map_subscribe_transient_local = LaunchConfiguration('map_subscribe_transient_local')
 
-    # List of nodes to be managed
     lifecycle_nodes = ['controller_server',
-                       'smoother_server',
                        'planner_server',
-                       'behavior_server',
+                       'recoveries_server',
                        'bt_navigator',
-                       #'velocity_smoother',
                        'waypoint_follower']
 
-    # Topic remappings
     remappings = [('/tf', 'tf'),
                   ('/tf_static', 'tf_static')]
-    
+
     # Create our own temporary YAML files that include substitutions
     param_substitutions = {
-        'use_sim_time':use_sim_time,
+        'use_sim_time': use_sim_time,
+        'default_bt_xml_filename': default_bt_xml_filename,
         'autostart': autostart,
-    }
+        'map_subscribe_transient_local': map_subscribe_transient_local}
 
-    # Prepare node parameters
-    configured_params = ParameterFile(
-        RewrittenYaml(
+    configured_params = RewrittenYaml(
             source_file=params_file,
-            root_key='',
+            root_key=namespace,
             param_rewrites=param_substitutions,
-            convert_types=True),
-        allow_substs=True)
+            convert_types=True)
 
-    # Set enviroment variable 
-    stdout_linebuf_envvar = SetEnvironmentVariable(
-        'RCUTILS_LOGGING_BUFFERED_STREAM', '1')
-    
-    # Declare the launch configuration
-    declare_params_cmd = DeclareLaunchArgument(
-        'params_file', default_value=os.path.join(package_name, 'config', 'nav_params.yaml'))
-    
-    declare_sim_time_cmd = DeclareLaunchArgument(
-        'use_sim_time', default_value='false',
-        description='Use simulation (Gazebo) clock if true')
-    
-    declare_autostart_cmd = DeclareLaunchArgument(
-        'autostart', default_value='True',
-        description='Automatically startup the navigation stack')
+    return LaunchDescription([
+        # Set env var to print messages to stdout immediately
+        SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
 
-    # Create a container
-    create_container = Node(
-        name='nav2_container',
-        package='rclcpp_components',
-        executable='component_container_isolated',
-        parameters=[configured_params, {'autostart': autostart}],
-        remappings=remappings,
-        output='screen')
+        DeclareLaunchArgument(
+            'namespace', default_value='',
+            description='Top-level namespace'),
 
-    # Load navigation nodes
-    load_composable_nodes = LoadComposableNodes(
-        target_container='nav2_container',
-        composable_node_descriptions=[
-            ComposableNode(
-                package='nav2_controller',
-                plugin='nav2_controller::ControllerServer',
-                name='controller_server',
-                parameters=[configured_params],
-                remappings=remappings), #+ [('cmd_vel', 'cmd_vel_nav')]
-            ComposableNode(
-                package='nav2_smoother',
-                plugin='nav2_smoother::SmootherServer',
-                name='smoother_server',
-                parameters=[configured_params],
-                remappings=remappings),
-            ComposableNode(
-                package='opennav_coverage',
-                plugin='opennav_coverage::CoverageServer',
-                name='coverage_server',
-                parameters=[configured_params],
-                remappings=remappings),
-            ComposableNode(
-                package='backported_bt_navigator',
-                plugin='backported_bt_navigator::BtNavigator',
-                name='bt_navigator',
-                parameters=[configured_params],
-                remappings=remappings),
-            ComposableNode(
-                package='nav2_behaviors',
-                plugin='behavior_server::BehaviorServer',
-                name='behavior_server',
-                parameters=[configured_params],
-                remappings=remappings + [('cmd_vel', 'cmd_vel_nav')],),
-            ComposableNode(
-                package='nav2_planner',
-                plugin='nav2_planner::PlannerServer',
-                name='planner_server',
-                parameters=[configured_params],
-                remappings=remappings),
-            ComposableNode(
-                package='nav2_velocity_smoother',
-                plugin='nav2_velocity_smoother::VelocitySmoother',
-                name='velocity_smoother',
-                parameters=[configured_params],
-                remappings=remappings +
-                           [('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'cmd_vel')]),
-            ComposableNode(
-                package='nav2_waypoint_follower',
-                plugin='nav2_waypoint_follower::WaypointFollower',
-                name='waypoint_follower',
-                parameters=[configured_params],
-                remappings=remappings),
-            ComposableNode(
-                package='nav2_lifecycle_manager',
-                plugin='nav2_lifecycle_manager::LifecycleManager',
-                name='lifecycle_manager_navigation',
-                parameters=[{'use_sim_time': use_sim_time,
-                             'autostart': autostart,
-                             'node_names': lifecycle_nodes}]),
-        ],
-    )
+        DeclareLaunchArgument(
+            'use_sim_time', default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
 
-    # Create the launch description and populate
-    ld = LaunchDescription([
-        # Launch Parameters
-        stdout_linebuf_envvar,
-        declare_params_cmd,
-        declare_sim_time_cmd,
-        declare_autostart_cmd,
-        
-        # Launch Nodes
-        create_container,
-        load_composable_nodes
+        DeclareLaunchArgument(
+            'autostart', default_value='true',
+            description='Automatically startup the nav2 stack'),
+
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=os.path.join(bringup_dir, 'config', 'nav_params.yaml'),
+            description='Full path to the ROS2 parameters file to use'),
+
+        DeclareLaunchArgument(
+            'default_bt_xml_filename',
+            default_value=os.path.join(
+                get_package_share_directory('nav2_bt_navigator'),
+                'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
+            description='Full path to the behavior tree xml file to use'),
+
+        DeclareLaunchArgument(
+            'map_subscribe_transient_local', default_value='false',
+            description='Whether to set the map subscriber QoS to transient local'),
+
+        Node(
+            package='nav2_controller',
+            executable='controller_server',
+            output='screen',
+            parameters=[configured_params],
+            remappings=remappings),
+
+        Node(
+            package='nav2_planner',
+            executable='planner_server',
+            name='planner_server',
+            output='screen',
+            parameters=[configured_params],
+            remappings=remappings),
+
+        Node(
+            package='nav2_recoveries',
+            executable='recoveries_server',
+            name='recoveries_server',
+            output='screen',
+            parameters=[configured_params],
+            remappings=remappings),
+
+        Node(
+            package='nav2_bt_navigator',
+            executable='bt_navigator',
+            name='bt_navigator',
+            output='screen',
+            parameters=[configured_params],
+            remappings=remappings),
+
+        Node(
+            package='nav2_waypoint_follower',
+            executable='waypoint_follower',
+            name='waypoint_follower',
+            output='screen',
+            parameters=[configured_params],
+            remappings=remappings),
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_navigation',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': autostart},
+                        {'node_names': lifecycle_nodes}]),
+
     ])
-    return ld
